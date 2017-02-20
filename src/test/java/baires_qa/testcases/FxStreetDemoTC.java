309ca4e8b3c1ee7d2d07c0b26ac55d9e23cfaf72
Atilio Vivern <atilio@bairesqa.com>
2017-02-19 21:36:35 -0300

    Primer Commit del Fmwk Que quedara como Demo

---
+++ src/test/java/baires_qa/testcases/FxStreetDemoTC.java
@@ -0,0 +1,31 @@
+package baires_qa.testcases;
+
+
+import baires_qa.utils.BaseTestCases;
+import baires_qa.utils.SuiteTrack;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.Test;
+
+
+/**
+ * Created by bairesqa on 10/1/2015.
+ * El Nombre del programa lo toma del nombre de la clase
+ *
+ */
+public class FxStreetDemoTC extends BaseTestCases {
+
+
+    @AfterSuite
+    public void report() {
+        new SuiteTrack(this.getClass().getSimpleName()).printReport();
+    }
+
+    @Test(dataProvider="BaseTestCases")
+    public void testCatalog( String testcaseName,String testActive, String browser,String keyword,
+                              String objectName,String objectType,String value, String comment)
+            throws Exception {
+
+       this.testLine( testcaseName,testActive,browser,keyword,objectName,objectType,value, comment);
+    }
+
+}
